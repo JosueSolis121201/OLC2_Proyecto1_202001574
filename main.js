e42e2da5d3e94a27b3bf545490151b90937b61cc
Josue <josuesolis121201@gmail.com>
2024-08-25 23:30:52 -0600

    Test
    prueba

---
+++ main.js
@@ -0,0 +1,35 @@
+import { parse } from "./parser/parser.js";
+import Environment from "./symbol/env.js";
+
+let executeBtn = document.getElementById("executeBtn");
+
+executeBtn.onclick = function () {
+  const codigo = `IdValido`;
+
+  const resultado = parse(codigo);
+  const global = new Environment(null);
+  
+  resultado.forEach((element) => {
+    element.execute(global);
+  });
+};
+
+
+const codigo = `/d* sdsfsdf;
+dfssd S*/`;
+const resultado = parse(codigo);
+console.log(resultado)
+
+// document.getElementById("input").addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     const texto = input.value;
+//     const resultado = parse(texto);
+//     const global = new Environment(null);
+//     for (let i = 0; i < resultado.length; i++) {
+//       resultado[i].execute(global);
+//     }
+//     console.log(resultado);
+//     resultado.execute();
+//     salida.innerHTML = "Resultado: " + resultado;
+//   }
+// });
